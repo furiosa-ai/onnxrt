f5a7324029338752806425b62f0c9170fab7ac97
Deokhwan Kim <dkim@furiosa.ai>
2022-05-20 16:22:36 +0900

    release: cut the v0.13.0 release

---
+++ 0.13.0/implementors/std/error/trait.Error.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["onnxrt"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.60.0/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"onnxrt/enum.Error.html\" title=\"enum onnxrt::Error\">Error</a>","synthetic":false,"types":["onnxrt::Error"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
