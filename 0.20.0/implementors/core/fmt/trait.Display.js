914f3552331d360dfb8e533333e4e2844f46ded4
Deokhwan Kim <dkim@furiosa.ai>
2023-06-19 15:44:25 +0900

    release: cut the v0.20.0 release

---
+++ 0.20.0/implementors/core/fmt/trait.Display.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"onnxrt":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.70.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"onnxrt/enum.Error.html\" title=\"enum onnxrt::Error\">Error</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
