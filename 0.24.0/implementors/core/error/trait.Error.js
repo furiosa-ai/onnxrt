931edb339bc94a8272949cd468270fa70372a12b
Deokhwan Kim <dkim@furiosa.ai>
2023-11-21 14:41:26 +0900

    release: cut the v0.24.0 release

---
+++ 0.24.0/implementors/core/error/trait.Error.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"onnxrt":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"onnxrt/enum.Error.html\" title=\"enum onnxrt::Error\">Error</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ 0.24.0/src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = JSON.parse('{\
+"onnxrt":["",[],["lib.rs"]]\
+}');
+createSrcSidebar();
